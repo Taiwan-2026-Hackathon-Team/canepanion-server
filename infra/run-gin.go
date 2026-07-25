77117d325ef7f7fa0a7ceac92c81344bbba13980
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2026-07-25 17:47:36 +0800

    🏛️infra: run gin server

---
+++ infra/run-gin.go
@@ -0,0 +1,38 @@
+package infra
+
+import (
+	"log"
+	"os"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+
+	"canepanion-server/middleware"
+)
+
+func RunGin(config cors.Config) {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	r := gin.Default()
+
+	r.Use(cors.New(config))
+
+	if os.Getenv("GIN_MODE") == "release" {
+		err := r.SetTrustedProxies([]string{"127.0.0.1"})
+		if err != nil {
+			log.Fatalf("Failed to set trusted proxies: %v", err)
+		}
+	}
+
+	r.Use(middleware.ErrorHandler())
+
+	RegisterRoutes(r, DB)
+
+	err := r.Run("0.0.0.0:" + port)
+	if err != nil {
+		log.Fatal("Failed to run the server")
+	}
+}
