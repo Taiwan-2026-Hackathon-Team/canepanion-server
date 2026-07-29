464dabf74a15ba16cb9e539f6e95d74d9244d988
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-07-29 09:44:57 +0800

    📂structure: internal/firmware_auth

---
+++ internal/firmware_auth/dto.go
@@ -0,0 +1,10 @@
+package firmwareauth
+
+type ActivateRequest struct{}
+type ActivateResponse struct{}
+
+type CreateSessionRequest struct{}
+type CreateSessionResponse struct{}
+
+type HeartbeatRequest struct{}
+type HeartbeatResponse struct{}
