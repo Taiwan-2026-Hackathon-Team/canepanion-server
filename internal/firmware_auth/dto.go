8548876e18fe9c79ba8fb67f2e9855dde1d88bd7
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-07-29 13:20:36 +0800

    🌐dto: activate device request & response struct

--- internal/firmware_auth/dto.go
+++ internal/firmware_auth/dto.go
@@ -1,7 +1,27 @@
 package firmwareauth
 
-type ActivateRequest struct{}
-type ActivateResponse struct{}
+import (
+	"time"
+
+	"canepanion-server/models"
+
+	"github.com/google/uuid"
+)
+
+type ActivateRequest struct {
+	DeviceID        string `json:"deviceId" binding:"required,uuid"`
+	BatteryLevel    int32  `json:"batteryLevel" binding:"min=0,max=100"`
+	FirmwareVersion string `json:"firmwareVersion" binding:"required,max=25"`
+}
+
+type ActivateResponse struct {
+	DeviceID        uuid.UUID           `json:"deviceId"`
+	Status          models.DeviceStatus `json:"status"`
+	BatteryLevel    int32               `json:"batteryLevel"`
+	FirmwareVersion string              `json:"firmwareVersion"`
+	LastSeenAt      time.Time           `json:"lastSeenAt"`
+	ServerTime      time.Time           `json:"serverTime"`
+}
 
 type CreateSessionRequest struct{}
 type CreateSessionResponse struct{}
