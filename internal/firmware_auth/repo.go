a069a648af5940124d1995378c1c7fde544bf8b7
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-07-29 13:24:17 +0800

    🌐api: find device by ID & update activation repo layer

--- internal/firmware_auth/repo.go
+++ internal/firmware_auth/repo.go
@@ -1,6 +1,11 @@
 package firmwareauth
 
-import "gorm.io/gorm"
+import (
+	"canepanion-server/models"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
 
 type Repository struct {
 	db *gorm.DB
@@ -8,4 +13,22 @@ type Repository struct {
 
 func NewRepository(db *gorm.DB) *Repository {
 	return &Repository{db: db}
+}
+
+func (r *Repository) FindDeviceByID(deviceID uuid.UUID) (*models.Devices, error) {
+	var device models.Devices
+	if err := r.db.First(&device, "id = ?", deviceID).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return &device, nil
+}
+
+func (r *Repository) UpdateActivation(device *models.Devices) error {
+	return r.db.Model(device).
+		Select("Status", "BatteryLevel", "FirmwareVersion", "LastSeenAt").
+		Updates(device).Error
 }
