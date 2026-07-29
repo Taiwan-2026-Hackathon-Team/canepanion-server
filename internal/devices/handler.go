61eef772938155b03de94a945a27e4b4560166dd
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-07-29 09:44:05 +0800

    📂structure: internal/devices

---
+++ internal/devices/handler.go
@@ -0,0 +1,22 @@
+package devices
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+type Handler struct {
+	service *Service
+}
+
+func NewHandler(db *gorm.DB) *Handler {
+	repo := NewRepository(db)
+	service := NewService(repo)
+	return &Handler{service: service}
+}
+
+func (h *Handler) Create(c *gin.Context) {
+	c.JSON(http.StatusNotImplemented, gin.H{"message": "create device is not implemented"})
+}
