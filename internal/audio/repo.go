1ff5d2dc1592e6fb20df28cce04fbbf4da1fc8dd
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2026-07-30 20:47:28 +0800

    🌐api: create audio repo layer

--- internal/audio/repo.go
+++ internal/audio/repo.go
@@ -1,6 +1,11 @@
 package audio
 
-import "gorm.io/gorm"
+import (
+	"canepanion-server/models"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
 
 type Repository struct {
 	db *gorm.DB
@@ -8,4 +13,19 @@ type Repository struct {
 
 func NewRepository(db *gorm.DB) *Repository {
 	return &Repository{db: db}
+}
+
+func (r *Repository) FindDeviceByID(deviceID uuid.UUID) (*models.Devices, error) {
+	var device models.Devices
+	if err := r.db.First(&device, "id = ?", deviceID).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &device, nil
+}
+
+func (r *Repository) CreateAudio(audio *models.Audio) error {
+	return r.db.Create(audio).Error
 }
