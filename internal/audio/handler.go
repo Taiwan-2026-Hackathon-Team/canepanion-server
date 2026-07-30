44e15cc5e96185d16fe0ebb706e1560262e9aff5
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2026-07-30 20:59:24 +0800

    🌐api: create upload handler layer

--- internal/audio/handler.go
+++ internal/audio/handler.go
@@ -3,6 +3,9 @@ package audio
 import (
 	"net/http"
 
+	appErr "canepanion-server/pkg/errors"
+	http_helper "canepanion-server/pkg/http"
+
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
@@ -18,7 +21,26 @@ func NewHandler(db *gorm.DB) *Handler {
 }
 
 func (h *Handler) CreateUpload(c *gin.Context) {
-	notImplemented(c, "create audio upload")
+	req, err := http_helper.BindFormJSON[CreateUploadRequest](c, "metadata")
+	if err != nil {
+		_ = c.Error(err)
+		return
+	}
+
+	file, fileHeader, err := c.Request.FormFile("audio")
+	if err != nil {
+		_ = c.Error(appErr.NewBadRequest("Audio file is required", err))
+		return
+	}
+	defer file.Close()
+
+	response, err := h.service.CreateUpload(c.Request.Context(), c.Param("deviceId"), req, file, fileHeader)
+	if err != nil {
+		_ = c.Error(err)
+		return
+	}
+
+	c.JSON(http.StatusCreated, response)
 }
 
 func (h *Handler) CompleteUpload(c *gin.Context) {
