efc6b761f37d85ef377a8cddf2c09860aaa48a5e
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2026-08-01 17:05:07 +0800

    🌐dto: get config response

--- internal/device_controls/dto.go
+++ internal/device_controls/dto.go
@@ -1,6 +1,22 @@
 package devicecontrol
 
-type GetConfigResponse struct{}
+import (
+	"encoding/json"
+
+	"github.com/google/uuid"
+)
+
+type DeviceConfiguration struct {
+	DeviceID      uuid.UUID
+	Version       uint64
+	Configuration json.RawMessage
+}
+
+type GetConfigResponse struct {
+	DeviceID      uuid.UUID       `json:"deviceId"`
+	Version       uint64          `json:"version"`
+	Configuration json.RawMessage `json:"configuration"`
+}
 
 type ListCommandsQuery struct{}
 type ListCommandsResponse struct{}
