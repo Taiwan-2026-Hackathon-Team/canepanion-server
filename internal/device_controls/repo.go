14f33edb9ef946b3ea12fea7751d4acea4be1648
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2026-08-01 17:05:27 +0800

    🌐api: find configuration by device ID repo layer

--- internal/device_controls/repo.go
+++ internal/device_controls/repo.go
@@ -1,6 +1,11 @@
 package devicecontrol
 
-import "gorm.io/gorm"
+import (
+	"canepanion-server/models"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
 
 type Repository struct {
 	db *gorm.DB
@@ -8,4 +13,21 @@ type Repository struct {
 
 func NewRepository(db *gorm.DB) *Repository {
 	return &Repository{db: db}
+}
+
+
+func (r *Repository) FindConfigurationByDeviceID(deviceID uuid.UUID) (*DeviceConfiguration, error) {
+	var config DeviceConfiguration
+	err := r.db.Model(&models.Devices{}).
+		Select("id AS device_id, configuration_version AS version, configuration").
+		Where("id = ?", deviceID).
+		Take(&config).Error
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return &config, nil
 }
